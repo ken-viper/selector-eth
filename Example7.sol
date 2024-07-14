100ae0c78d554893bb59ba5d0422eb7df2d02f42
ymcorrea <botbenz111@gmail.com>
2024-07-14 12:46:10 -0400

    Update Example7.sol

--- Example7.sol
+++ Example7.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.25; // Specify the version of Solidity to be used
 // Define the CollisionExample contract
 contract CollisionExample {
     // Function collate_propagate_storage with a parameter of type bytes16
-    function collate_propagate_storage(bytes16 x) external {}
+    function collate_propagate_storage(bytes16 x) external {} // has selector "0x42966c68"
     
     // Function burn with a parameter of type uint256
-    function burn(uint256 amount) external {}
+    function burn(uint256 amount) external {} // also has selector "0x42966c68" --> clash with burn!
     
     // This contract will not compile due to a hash collision between the function selectors.
     // The function selectors for both collate_propagate_storage and burn result in the same hash,
