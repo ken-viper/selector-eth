955013a521dce480731acf3f9511785d6e8968e6
ymcorrea <botbenz111@gmail.com>
2024-07-14 12:44:51 -0400

    Create Example7.sol

---
+++ Example7.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.25; // Specify the version of Solidity to be used
+
+// Define the CollisionExample contract
+contract CollisionExample {
+    // Function collate_propagate_storage with a parameter of type bytes16
+    function collate_propagate_storage(bytes16 x) external {}
+    
+    // Function burn with a parameter of type uint256
+    function burn(uint256 amount) external {}
+    
+    // This contract will not compile due to a hash collision between the function selectors.
+    // The function selectors for both collate_propagate_storage and burn result in the same hash,
+    // causing a collision and preventing the contract from being compiled.
+}
